b323bac802647ff195bd982c46c7ee0494b49dae
ivanmikhaylov1 <121641841+ivanmikhaylov1@users.noreply.github.com>
2024-11-14 19:51:39 +0300

    Обновлен build.gradle.kts
    Добавлены новые зависимости

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("java")
+    application
+    id("pmd")
+    id("jacoco")
 }
 
 group = "org.example"
@@ -12,6 +15,16 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
+    implementation("org.slf4j:slf4j-api:1.7.32")
+    implementation("ch.qos.logback:logback-classic:1.2.6")
+}
+pmd {
+    isConsoleOutput = true
+    toolVersion = "7.0.0"
+    rulesMinimumPriority = 5
+    ruleSets = listOf("category/java/errorprone.xml", "category/java/bestpractices.xml")
+    threads = 6
 }
 
 tasks.test {
