1b66d45dc208e4ea9d1b92e0fe278e0ba74f8d4e
margarita-turkina <turkina.ms@phystech.edu>
2025-01-18 00:34:32 +0300

    Create BlogParserManager.java

---
+++ src/main/java/org/example/parser/BlogParserManager.java
@@ -0,0 +1,29 @@
+package org.example.parser;
+
+import org.example.parser.sites.InfoqParser;
+import org.example.parser.sites.ThreeDNewsParser;
+import org.example.parser.sites.HiTechParser;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BlogParserManager {
+  private final Map<String, SiteParse> parsers = new HashMap<>();
+
+  public BlogParserManager() {
+    parsers.put("infoq", new InfoqParser()); // Идентификатор для сайта Infoq
+    parsers.put("3dnews", new ThreeDNewsParser()); // Добавить сюда другие парсеры
+    parsers.put("hitech", new HiTechPaser());
+  }
+
+  public List<Article> parse(String siteId) {
+    SiteParse parser = parsers.get(siteId);
+
+    if (parser != null) {
+      return parser.parseLastArticles();
+    } else {
+      throw new IllegalArgumentException("Парсер для указанного сайта не найден: " + siteId);
+    }
+  }
+}
