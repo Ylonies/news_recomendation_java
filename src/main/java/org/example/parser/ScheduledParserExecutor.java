ab91c47e1962598ac933975b7d4aebd53718ea3c
margarita-turkina <turkina.ms@phystech.edu>
2025-01-18 00:35:58 +0300

    Create ScheduledParserExecutor.java

---
+++ src/main/java/org/example/parser/ScheduledParserExecutor.java
@@ -0,0 +1,16 @@
+package org.example.parser;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public record ScheduledParserExecutor(BlogParserManager blogParserManager) {
+  private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+  public void scheduleParsing(String siteId) {
+    scheduler.scheduleAtFixedRate(() -> {
+      blogParserManager.parse(siteId);
+      // TODO: сохранение статей в БД
+    }, 0, 5, TimeUnit.MINUTES);
+  }
+}
